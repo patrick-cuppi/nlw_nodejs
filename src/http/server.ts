93998c6b4dc4e3b265a7e641c0ad69742f0f8811
Patrick Cuppi <cuppi.patrick@gmail.com>
2024-02-11 13:39:04 -0300

    feat: created options in createPoll

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,26 +1,9 @@
 import fastify from "fastify"
-import { PrismaClient } from "@prisma/client"
-import { z } from "zod"
+import { createPoll } from './routes/create-polls'
 
 const app = fastify()
 
-const prisma = new PrismaClient()
-
-app.post('/polls', async (request) => {
-    const createPollBody = z.object({
-        title : z.string()
-    })
-    
-    const { title } = createPollBody.parse(request.body)
-
-    const poll = await prisma.poll.create({
-        data : {
-            title,
-        }
-    })
-    
-    return poll
-})
+app.register(createPoll)
 
 app.listen({ port:3333 }).then(() => {
     console.log('HTTP Server Running!')
