ee854ba7ea4d58dfc74753e31331868c3a2a0c6c
Patrick Cuppi <cuppi.patrick@gmail.com>
2024-02-11 15:22:32 -0300

    feat: created route vote-on-polls

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,11 +1,19 @@
 import fastify from "fastify"
+import cookie from '@fastify/cookie'
 import { createPoll } from './routes/create-polls'
 import { getPoll } from "./routes/get-polls"
+import { voteOnPoll } from "./routes/vote-on-poll"
 
 const app = fastify()
 
+app.register(cookie, {
+    secret: "app-polls-websocket",
+    hook: 'onRequest',
+})
+
 app.register(createPoll)
 app.register(getPoll)
+app.register(voteOnPoll)
 
 app.listen({ port:3333 }).then(() => {
     console.log('HTTP Server Running!')
